31efaac514cf551d32c720e87fba267c6f8ae149
chanyoung <dev-chanyoung@gmail.com>
2025-11-20 11:07:00 +0900

    초기 설정

---
+++ src/main/java/dev/chanyoung/myhistory/MyHistoryApplication.java
@@ -0,0 +1,11 @@
+package dev.chanyoung.myhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHistoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyHistoryApplication.class,args);
+    }
+}
